6e3564fc894251040a78592eee4c7baf8cb5c5d8
Tony White <vshierdom@gmail.com>
2023-12-01 15:46:08 -1000

    trying different app structure

--- app/main.py
+++ app/main.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template, send_file, jsonify
 import requests
 import csv
 import time
+import threading
+
 from datetime import datetime
 
 app = Flask(__name__, static_url_path="/static", static_folder="static")
 
 # Global variable to control the logging thread
 logging_active = False
-distance_url = 'http://localhost:6040/mavlink/vehicles/1/components/194/messages/DISTANCE_SENSOR'
-gps_url = 'http://localhost:6040/mavlink/vehicles/1/components/1/messages/GLOBAL_POSITION_INT'
+distance_url = 'http://192.168.1.52:6040/mavlink/vehicles/1/components/194/messages/DISTANCE_SENSOR'
+gps_url = 'http://192.168.1.52:6040/mavlink/vehicles/1/components/1/messages/GLOBAL_POSITION_INT'
 log_file = 'sensor_data.csv'
 log_rate = 2
 
@@ -18,42 +20,9 @@ feedback_interval = 5
 
 # Initialize a counter for the number of rows added
 row_counter = 0
-@app.route('/')
-def home():
-    return app.send_static_file("index.html")
-
-@app.route('/start')
-def start_logging():
-    global logging_active
-    if not logging_active:
-        logging_active = True
-        main()  # Start the logging script directly
-    return 'Started'
-
-@app.route("/get_data")
-def get_data():
-    data = {
-        'unix_timestamp': unix_timestamp,
-        'distance': distance,
-        'latitude': latitude,
-        'longitude': longitude
-    }
-    return jsonify(data)
-
-@app.route('/stop')
-def stop_logging():
-    global logging_active
-    logging_active = False
-    return 'Stopped'
-
-@app.route('/download')
-def download_file():
-    return send_file('sensor_data.csv', as_attachment=True)
-
-def main():
-    # Initialize a counter for the number of rows added
-    row_counter = 0
-
+data = []
+def fetch_data():
+    global data
     # Main loop for logging data
     while (logging_active == True):
         try:
@@ -72,18 +41,14 @@ def main():
 
                 # Extract the values for each column
                 timestamp = int(time.time() * 1000)  # Convert current time to milliseconds
-                dt = datetime.fromtimestamp(timestamp / 1000)  # Convert timestamp to datetime object
-                global unix_timestamp
+                dt = datetime.fromtimestamp(timestamp / 1000)  # Convert timestamp to datetime object 
                 unix_timestamp = timestamp
                 year, month, day, hour, minute, second = dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
-                global distance
                 distance = distance_data['current_distance']
-                global latitude
                 latitude = gps_data['lat'] / 1e7
-                global longitude
                 longitude = gps_data['lon'] / 1e7
                 column_values = [unix_timestamp, year, month, day, hour, minute, second, distance, latitude, longitude]
-
+                data = column_values
                 # Create or append to the log file and write the data
                 with open(log_file, 'a', newline='') as csvfile:
                     writer = csv.writer(csvfile)
@@ -114,6 +79,30 @@ def main():
         except Exception as e:
             print(f"An error occurred: {e}")
             break
+
+@app.route('/')
+def home():
+    return app.send_static_file("index.html")
+def get_data():
+    return jsonify(data)
+
+@app.route('/start')
+def start_logging():
+    global logging_active
+    if not logging_active:
+        logging_active = True
+        threading.Thread(target=fetch_data).start()# Start the logging script directly
+    return 'Started'
+
+@app.route('/stop')
+def stop_logging():
+    global logging_active
+    logging_active = False
+    return 'Stopped'
+
+@app.route('/download')
+def download_file():
+    return send_file('sensor_data.csv', as_attachment=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
