0d188ba301aea7bdac631741cb4b1ea4f1b2bd99
Tony White <tony.white@foreveroceans.com>
2023-12-22 16:36:10 -1000

    found it? wrong url for attitude

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = Flask(__name__, static_url_path="/static", static_folder="static") #setup 
 logging_active = False# Global variable to control the logging 
 distance_url = 'http://host.docker.internal/mavlink2rest/mavlink/vehicles/1/components/194/messages/DISTANCE_SENSOR' #10.144.19.16
 gps_url = 'http://host.docker.internal/mavlink2rest/mavlink/vehicles/1/components/1/messages/GLOBAL_POSITION_INT'
-yaw_url= 'http://host.docker.internal/mavlink/vehicles/1/components/1/messages/ATTITUDE'
+yaw_url= 'http://host.docker.internal/mavlink2rest/mavlink/vehicles/1/components/1/messages/ATTITUDE'
 log_file = 'sensordata.csv'
 log_rate = 2 #Desired rate in Hz
 data = []
