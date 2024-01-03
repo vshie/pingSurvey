c77047cc89adba027689516f191dd41c125d8600
Tony White <tony.white@foreveroceans.com>
2024-01-02 18:23:05 -1000

    update map icon, give up on background image

--- app/main.py
+++ app/main.py
@@ -91,10 +91,10 @@ def servicenames():
     return '''
     {"name": "Simple Ping2 Survey ",
     "description": "This extension makes it easy to record data from the ping2 sonar onbaord the vehicle, keeping a poor communications link from interfering with the quality of collected survey data. When connected, the extension displays a data preview that is intended to aide in survey grid spacing determination. Happy motoring!",
-    "icon": "mdiMapCheck",
+    "icon": "map-check-outline",
     "company": "Blue Robotics",
     "version": "0.0.1",
-    "webpage": "https://github.com/bluerobotics/BlueOS-docker",
+    "webpage": "https://github.com/vshie/pingSurvey",
     "api": "https://github.com/bluerobotics/BlueOS-docker"}
     '''
     
